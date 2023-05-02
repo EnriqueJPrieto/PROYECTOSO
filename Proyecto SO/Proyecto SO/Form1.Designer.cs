da1a4217c0cb349e7c29bf8e01c2e55cc8f5bb2b
Enrique J Prieto <kikeprietobailo@gmail.com>
2023-05-02 23:26:17 +0200

    V4

--- Proyecto SO/Proyecto SO/Form1.Designer.cs
+++ Proyecto SO/Proyecto SO/Form1.Designer.cs
@@ -44,6 +44,15 @@
             this.CONECTADOS = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label4 = new System.Windows.Forms.Label();
+            this.ACEPTAR = new System.Windows.Forms.Button();
+            this.RECHAZAR = new System.Windows.Forms.Button();
+            this.INVITAR = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -109,21 +118,21 @@
             // 
             // NOMBRE
             // 
-            this.NOMBRE.Location = new System.Drawing.Point(144, 63);
+            this.NOMBRE.Location = new System.Drawing.Point(144, 84);
             this.NOMBRE.Name = "NOMBRE";
             this.NOMBRE.Size = new System.Drawing.Size(100, 23);
             this.NOMBRE.TabIndex = 6;
             // 
             // CONTRASEÑA
             // 
-            this.CONTRASEÑA.Location = new System.Drawing.Point(144, 92);
+            this.CONTRASEÑA.Location = new System.Drawing.Point(144, 113);
             this.CONTRASEÑA.Name = "CONTRASEÑA";
             this.CONTRASEÑA.Size = new System.Drawing.Size(100, 23);
             this.CONTRASEÑA.TabIndex = 7;
             // 
             // PARAMETRO
             // 
-            this.PARAMETRO.Location = new System.Drawing.Point(144, 122);
+            this.PARAMETRO.Location = new System.Drawing.Point(144, 142);
             this.PARAMETRO.Name = "PARAMETRO";
             this.PARAMETRO.Size = new System.Drawing.Size(100, 23);
             this.PARAMETRO.TabIndex = 8;
@@ -131,7 +140,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 63);
+            this.label1.Location = new System.Drawing.Point(27, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 15);
             this.label1.TabIndex = 9;
@@ -140,7 +149,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 92);
+            this.label2.Location = new System.Drawing.Point(27, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 15);
             this.label2.TabIndex = 10;
@@ -149,7 +158,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 121);
+            this.label3.Location = new System.Drawing.Point(27, 150);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 15);
             this.label3.TabIndex = 11;
@@ -157,9 +166,9 @@
             // 
             // CONNECT
             // 
-            this.CONNECT.Location = new System.Drawing.Point(369, 34);
+            this.CONNECT.Location = new System.Drawing.Point(27, 12);
             this.CONNECT.Name = "CONNECT";
-            this.CONNECT.Size = new System.Drawing.Size(75, 23);
+            this.CONNECT.Size = new System.Drawing.Size(103, 48);
             this.CONNECT.TabIndex = 12;
             this.CONNECT.Text = "CONECTAR";
             this.CONNECT.UseVisualStyleBackColor = true;
@@ -179,21 +188,105 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Highlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(595, -1);
+            this.dataGridView1.Location = new System.Drawing.Point(644, -1);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(203, 449);
+            this.dataGridView1.Size = new System.Drawing.Size(154, 449);
             this.dataGridView1.TabIndex = 14;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 187);
+            this.label4.Location = new System.Drawing.Point(27, 300);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 15);
             this.label4.TabIndex = 15;
             this.label4.Text = "label4";
+            // 
+            // ACEPTAR
+            // 
+            this.ACEPTAR.Location = new System.Drawing.Point(27, 350);
+            this.ACEPTAR.Name = "ACEPTAR";
+            this.ACEPTAR.Size = new System.Drawing.Size(75, 23);
+            this.ACEPTAR.TabIndex = 16;
+            this.ACEPTAR.Text = "button1";
+            this.ACEPTAR.UseVisualStyleBackColor = true;
+            this.ACEPTAR.Click += new System.EventHandler(this.ACEPTAR_Click);
+            // 
+            // RECHAZAR
+            // 
+            this.RECHAZAR.Location = new System.Drawing.Point(144, 350);
+            this.RECHAZAR.Name = "RECHAZAR";
+            this.RECHAZAR.Size = new System.Drawing.Size(75, 23);
+            this.RECHAZAR.TabIndex = 17;
+            this.RECHAZAR.Text = "button1";
+            this.RECHAZAR.UseVisualStyleBackColor = true;
+            this.RECHAZAR.Click += new System.EventHandler(this.RECHAZAR_Click);
+            // 
+            // INVITAR
+            // 
+            this.INVITAR.AutoSize = true;
+            this.INVITAR.Location = new System.Drawing.Point(495, 65);
+            this.INVITAR.Name = "INVITAR";
+            this.INVITAR.Size = new System.Drawing.Size(83, 19);
+            this.INVITAR.TabIndex = 18;
+            this.INVITAR.Text = "checkBox1";
+            this.INVITAR.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(495, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(206, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(206, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 15);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(206, 237);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 15);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(206, 262);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 15);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "label10";
             // 
             // Form1
             // 
@@ -201,6 +294,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.INVITAR);
+            this.Controls.Add(this.RECHAZAR);
+            this.Controls.Add(this.ACEPTAR);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.CONECTADOS);
@@ -244,5 +346,14 @@
         private Button CONECTADOS;
         private DataGridView dataGridView1;
         private Label label4;
+        private Button ACEPTAR;
+        private Button RECHAZAR;
+        private CheckBox INVITAR;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
     }
 }
---
+++ readme.txt
@@ -0,0 +1,44 @@
+VERSION 4:
+----------------------------------------------------
+GENERADO POR:   Prieto Bailo Enrique José
+
+VERIFICADO POR: Castellon Aguilar Cristopher
+
+VIDEOS POR:      Yébenes García Rubén
+
+LINK VIDEO:      
+
+VERSION 3:
+----------------------------------------------------
+
+GENERADO POR:   Castellon Aguilar Cristopher
+
+VERIFICADO POR: Yébenes García Rubén
+
+VIDEOS POR:      Prieto Bailo Enrique José
+
+LINK VIDEO:      https://youtu.be/C7I1g07wtR0
+
+INTERFAZ GRAFICA:   https://youtu.be/3l13jddIAbE
+
+VERSION 2:
+----------------------------------------------------
+
+GENERADO POR:   Yébenes García Rubén
+
+VERIFICADO POR: Prieto Bailo Enrique José
+
+VIDEO POR:      Castellon Aguilar Cristopher
+
+LINK VIDEO:     https://youtu.be/Z-uCbVzxjNI
+
+VERSION 1:
+----------------------------------------------------
+
+GENERADO POR:   Prieto Bailo Enrique José
+
+VERIFICADO POR: Castellon Aguilar Cristopher
+
+VIDEO POR:      Yébenes García Rubén
+
+LINK VIDEO:     https://drive.google.com/file/d/1ejmwK2fNMldgwXfLJOAEjA2ch9rWHFlg/view?usp=share_link
